225e2f5ef24fa36cf226cc5e980014159461de68
Никита Фёдоров <remtex133@gmail.com>
2021-11-15 21:32:07 +0600

    Add Class(meneger) and add Frame in MainFrame.xaml

---
+++ IAPP/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace IAPP
+{
+    internal class Manager
+    {
+        public static Frame MEF { get; set; }
+    }
+}
