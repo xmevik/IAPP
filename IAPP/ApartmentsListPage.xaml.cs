49237ea4c6cac8d10c93cfd3843815f3bdf612b2
Сергей Аверин <averin.0503@gmail.com>
2021-11-21 13:57:34 +0600

    Added some filters and added dlete button

--- IAPP/ApartmentsListPage.xaml.cs
+++ IAPP/ApartmentsListPage.xaml.cs
@@ -27,8 +27,18 @@ namespace IAPP
         public ApartmentsListPage()
         {
             InitializeComponent();
-            var apartments = BaseDomNSLEEntities.GetContext().Apartaments.ToList();
-            LViewApartments.ItemsSource = apartments;
+
+            var residentialComplexs = BaseDomNSLEEntities.GetContext().ResidentialComplex.ToList();
+            residentialComplexs.Insert(0, new ResidentialComplex { Name = "Все жилищные комплекса" });
+            var houses = BaseDomNSLEEntities.GetContext().House.ToList();
+            houses.Insert(0, new House { ID = 0 });
+
+            houseComboBox.ItemsSource = houses;
+            residentialComplexComboBox.ItemsSource = residentialComplexs;
+            residentialComplexComboBox.SelectedIndex = 0;
+            houseComboBox.SelectedIndex = 0;
+
+            updateApartmentsList();
         }
 
         private void StackPanel_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
@@ -53,6 +63,70 @@ namespace IAPP
             filterBtn.Visibility = Visibility.Visible;
             pagginateButtonsWrapPanel.Visibility = Visibility.Visible;
 
+        }
+
+        private void residentialComplexComboBox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+           
+        }
+
+        private void residentialComplexComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var houses = BaseDomNSLEEntities.GetContext().House.ToList();
+
+            if (residentialComplexComboBox.SelectedIndex > 0)
+                houses = houses.Where((item) => item.ResidentialComplexID == residentialComplexComboBox.SelectedIndex).ToList();
+
+            houses.Insert(0, new House { ID = 0 });
+            houseComboBox.ItemsSource = houses;
+
+            updateApartmentsList();
+        }
+        private void updateApartmentsList()
+        {
+            var apartments = BaseDomNSLEEntities.GetContext().Apartaments.ToList();
+
+            if (houseComboBox.SelectedIndex > 0)
+                apartments = apartments.Where((item) => item.HouseID == houseComboBox.SelectedIndex).ToList();
+
+            if (floorTextBox.Text != "")
+                apartments = apartments.Where((item) => item.Floor.ToString() == floorTextBox.Text).ToList();
+
+            if (sectionTextBox.Text != "")
+                apartments = apartments.Where((item) => item.Section.ToString() == sectionTextBox.Text).ToList();
+
+            LViewApartments.ItemsSource = apartments;
+        }
+
+        private void houseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateApartmentsList();
+        }
+
+        private void floorTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateApartmentsList();
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var HotelForRemove = LViewApartments.SelectedItems.Cast<Apartaments>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {HotelForRemove.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    BaseDomNSLEEntities.GetContext().Apartaments.RemoveRange(HotelForRemove);
+                    BaseDomNSLEEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+
+                    LViewApartments.ItemsSource = BaseDomNSLEEntities.GetContext().Apartaments.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
         }
     }
 }
